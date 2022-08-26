be2bfc4abb65070e04033112c5026d5a43c27779
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-08-25 22:03:18 -0300

    INSERINDO CAMPOS DA CLASSE ALUNO

--- DiarioEletronico/Model/Aluno.cs
+++ DiarioEletronico/Model/Aluno.cs
@@ -8,6 +8,28 @@ namespace Model
 {
     public class Aluno
     {
+        private int id;
+        private int id_Sexo;
+
+        public int Matricula //Id do Aluno
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Id_Sexo
+        {
+            get { return id_Sexo; }
+            set { id_Sexo = value; }
+        }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
 
     }
 }
