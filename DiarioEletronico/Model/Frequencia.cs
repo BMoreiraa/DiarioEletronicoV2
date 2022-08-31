3e7ba35f9d1c87e14e5ed9fd0a682ea64a9c3868
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-08-31 14:29:38 -0300

    Classe model finalizada.

---
+++ DiarioEletronico/Model/Frequencia.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Frequencia
+    {
+        private int id;
+        private int id_Aluno;
+        private int id_Diario;
+        private bool faltas;
+        private string data_;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int Id_Aluno
+        {
+            get { return id_Aluno; }
+            set { id_Aluno = value; }
+        }
+
+        public int Id_Diario
+        {
+            get { return id_Diario; }
+            set { id_Diario = value; }
+        }
+
+        public bool Faltas
+        {
+            get { return faltas; }
+            set { faltas = value; }
+        }
+
+        public string Data_
+        {
+            get { return data_; }
+            set { data_ = value; }
+        }
+
+    }
+}
---
+++ DiarioEletronico/Model/UF.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class UF
+    {
+        private int id;
+        private string nomeUF;
+        private string abreviacaoUF;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string NomeUF
+        {
+            get { return nomeUF; }
+            set { nomeUF = value; }
+        }
+
+        public string AbreviacaoUF
+        {
+            get { return abreviacaoUF; }
+            set { abreviacaoUF = value; }
+        }
+
+    }
+}
