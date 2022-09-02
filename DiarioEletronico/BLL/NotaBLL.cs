ddaeb98d980be07bf9eb8b7e71311b9f3536a9bb
Bruno Alencar Moreira <90804306+BMoreiraa@users.noreply.github.com>
2022-09-02 20:37:00 -0300

    BLL
    Classes BLL de NOTA, OCORRENCIA, PLANO DE AULA, PROFESSOR, SEXO, TURMA, TURNO E UF criadas com apenas o método inserir.

---
+++ DiarioEletronico/BLL/NotaBLL.cs
@@ -0,0 +1,15 @@
+﻿using DAL;
+using Model;
+
+namespace BLL
+{
+    public class NotaBLL
+    {
+        public Nota Inserir(Nota _nota)
+        {
+
+            NotaDAL notaDAL = new NotaDAL();
+            return notaDAL.Inserir(_nota);
+        }
+    }
+}
