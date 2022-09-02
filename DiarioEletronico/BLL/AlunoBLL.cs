5f64ea55fcf1f3ca86c37519c439696a28b7431a
Bruno Henrique <90804273+BrunoHenrique80@users.noreply.github.com>
2022-09-02 13:24:34 -0300

    Classe BLL
    Classes BLL de Agente, aluno, bimestre, cidade, diário, disciplina e frequência criada com apenas o método inserir.

--- DiarioEletronico/BLL/AlunoBLL.cs
+++ DiarioEletronico/BLL/AlunoBLL.cs
@@ -1,6 +1,14 @@
-﻿namespace BLL
+﻿using DAL;
+using Model;
+
+namespace BLL
 {
     public class AlunoBLL
     {
+        public Aluno Inserir(Aluno aluno)
+        {
+           AlunoDAL alunoDAL = new AlunoDAL();
+            return alunoDAL.Inserir(aluno);
+        }
     }
 }
---
+++ DiarioEletronico/BLL/DiarioBLL.cs
@@ -0,0 +1,14 @@
+﻿using DAL;
+using Model;
+
+namespace BLL
+{
+    public class DiarioBLL
+    {
+        public Diario Inserir(Diario diario)
+        {
+            DiarioDAL diarioDAL = new DiarioDAL();
+            return diarioDAL.Inserir(diario);
+        }
+    }
+}
