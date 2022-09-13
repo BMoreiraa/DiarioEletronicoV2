20b781d474f73a5ea2dd7011504439f259032168
Bruno Alencar Moreira <90804306+BMoreiraa@users.noreply.github.com>
2022-09-12 21:45:21 -0300

    teste

--- DiarioEletronico/Infra/Arquivo.cs
+++ DiarioEletronico/Infra/Arquivo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,17 @@ namespace Infra
 {
     public class Arquivo
     {
+        public static ArrayList BuscarConecoesBD()
+        {
+            ArrayList retornoArray = new ArrayList();
+
+            string[] caminho = Directory.GetFiles(Constante.DiretorioDoEnderecoBanco, "BD*");
+            for(int i = 0; i < caminho.Length; i++)
+            {
+                string conteudo = File.ReadAllText(caminho[i]);
+                retornoArray.Add(FuncoesGlobais.Base64Decode(conteudo));
+            }
+            return retornoArray;
+        }
     }
 }
--- DiarioEletronico/Infra/Constante.cs
+++ DiarioEletronico/Infra/Constante.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Infra
 {
-    class Constante
+    public class Constante
     {
+        public static string DiretorioDoEnderecoBanco = Environment.CurrentDirectory + @"\Servididor\";
+        public static string NomeArquivoBanco = "Endereço do servido.txt";
+    }
+
+        public static void GravaStringDeConexaoNoArquivo(string _texto)
+        {
+            string nomeArquivo = "BD";
+
+            if (!Directory.Exists(Constante.DiretorioDoEnderecoBanco))
+            Directory.CreateDirectory(Constante.DiretorioDoEnderecoBanco);
+
+        for (int i = 1; i > 0; i++)
+        {
+            if (!File.Exists(Constante.DiretorioDoEnderecoBanco + nomeArquivo + i + ".txt"))
+            {
+                FileStream fileStream = new FileStream(Constante.DiretorioDoEnderecoBanco + nomeArquivo + ".txt", FileMode.Create);
+
+            }
+        }
+
     }
 }
