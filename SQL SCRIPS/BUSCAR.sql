99f1dcd0a449b86193684845f8be8832381d0f54
Bruno Alencar Moreira <90804306+BMoreiraa@users.noreply.github.com>
2022-09-19 20:20:48 -0300

    ..

--- SQL SCRIPS/BUSCAR.sql
+++ SQL SCRIPS/BUSCAR.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE SP_BuscarAgente
 	@filtro varchar(250) = ''
 AS
-	SELECT Id,Senha,NomeAgente,NomeUsuario from AgentePedagogico WHERE  Id like '%' +@filtro + '%' OR NomeUsuario LIKE  '' + @filtro + ''
+	SELECT Id,Senha,NomeAgente,NomeUsuario from AgentePedagogico WHERE  Id like '%' +@filtro + '%' OR NomeUsuario LIKE  + @filtro + '%'
 GO----OK
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 CREATE PROCEDURE SP_BuscarAluno /*buscando por nome, id, NOME DE USUARIO */
@@ -14,5 +14,5 @@ GO
 CREATE PROC SP_BuscarProfessor
  @filtro varchar(250) = ''
 AS
-	SELECT Id,Id_Sexo,Id_Cidade,Nome,CPF,Email,Telefone,DataDeNascimento,Endereco,Complemento,setor,CEP,Senha,NomeUsuario from Professor WHERE Id '%' + LIKE  @filtro + '%' OR NomeUsuario like + @filtro+'%'
+	SELECT Id,Id_Sexo,Id_Cidade,Nome,CPF,Email,Telefone,DataDeNascimento,Endereco,Complemento,setor,CEP,Senha,NomeUsuario from Professor WHERE Id LIKE '' + @filtro + '%' OR NomeUsuario like + @filtro+'%'
 GO
